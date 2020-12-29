4b1f37e2ad6f4db7af8e42366a0530a2b5433b3c
Niklas Arnitz <niklasarnitz@swdec.de>
2020-12-29 15:01:33 +0100

    Update map

---
+++ tiles_png/BuildingBlocks.tsx
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="BuildingBlocks" tilewidth="512" tileheight="512" tilecount="72" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="community/cert/imgs/leipzig_messe/01_stone_tile1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="community/cert/imgs/leipzig_messe/02_stone_tile2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="96" source="community/cert/imgs/leipzig_messe/03_window_door1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="96" source="community/cert/imgs/leipzig_messe/05_window_wall1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="96" source="community/cert/imgs/leipzig_messe/06_window_wall_1_blinds.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="96" source="community/cert/imgs/leipzig_messe/07_window_wall_1_frame.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="96" source="community/cert/imgs/leipzig_messe/08_window_wall_1_window.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="community/cert/imgs/leipzig_messe/09_window_wall_2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="community/cert/imgs/leipzig_messe/10_wood_floor2.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="32" source="community/cert/imgs/leipzig_messe/11_window_door2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="96" source="community/cert/imgs/leipzig_messe/12_window_wall_1_window2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="community/cert/imgs/leipzig_messe/13_stone_tile_hatch.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="128" source="community/cert/imgs/leipzig_messe/14_tresen.png"/>
+ </tile>
+ <tile id="13">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_0.png"/>
+ </tile>
+ <tile id="14">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_0.png"/>
+ </tile>
+ <tile id="15">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_4.png"/>
+ </tile>
+ <tile id="19">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_5.png"/>
+ </tile>
+ <tile id="20">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_6.png"/>
+ </tile>
+ <tile id="21">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_7.png"/>
+ </tile>
+ <tile id="22">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_8.png"/>
+ </tile>
+ <tile id="23">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/1_Palme_1_9.png"/>
+ </tile>
+ <tile id="24">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_0.png"/>
+ </tile>
+ <tile id="25">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_1.png"/>
+ </tile>
+ <tile id="26">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_3.png"/>
+ </tile>
+ <tile id="28">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_0.png"/>
+ </tile>
+ <tile id="29">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_3.png"/>
+ </tile>
+ <tile id="32">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_4.png"/>
+ </tile>
+ <tile id="33">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_5.png"/>
+ </tile>
+ <tile id="34">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_6.png"/>
+ </tile>
+ <tile id="35">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_7.png"/>
+ </tile>
+ <tile id="36">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_8.png"/>
+ </tile>
+ <tile id="37">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_9.png"/>
+ </tile>
+ <tile id="38">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_4_10.png"/>
+ </tile>
+ <tile id="39">
+  <image width="160" height="96" source="community/cert/imgs/c3_deko/2_Rundzelt_beleuchtet.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/03_RKL_blau_blink1.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/03_RKL_blau_blink2.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/03_RKL_blau_blink3.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/03_RKL_blau_blink4.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/04_RKL_rot_blink1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/04_RKL_rot_blink2.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/04_RKL_rot_blink3.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="community/cert/imgs/c3_deko/04_RKL_rot_blink4.png"/>
+ </tile>
+ <tile id="48">
+  <image width="128" height="64" source="community/cert/imgs/doors/01_door.png"/>
+ </tile>
+ <tile id="49">
+  <image width="128" height="64" source="community/cert/imgs/doors/02_door_frame.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="64" source="community/cert/imgs/doors/03_tuer_bullauge_rechts_blau.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="64" source="community/cert/imgs/doors/04_tuer_bullauge_rechts_gelb.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="64" source="community/cert/imgs/doors/05_tuer_bullauge_rechts_gruen.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="64" source="community/cert/imgs/doors/06_tuer_bullauge_rechts_lila.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="community/cert/imgs/doors/07_tuer_doppel.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="community/cert/imgs/doors/08_tuer_doppel_blau.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="64" source="community/cert/imgs/doors/09_tuer_solid_links_braun.png"/>
+ </tile>
+ <tile id="57">
+  <image width="32" height="64" source="community/cert/imgs/doors/10_tuer_standard_links.png"/>
+ </tile>
+ <tile id="58">
+  <image width="32" height="64" source="community/cert/imgs/doors/11_tuer_standard_links_blau.png"/>
+ </tile>
+ <tile id="59">
+  <image width="32" height="64" source="community/cert/imgs/doors/12_tuer_standard_links_braun.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="64" source="community/cert/imgs/doors/13_tuer_standard_rechts.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="64" source="community/cert/imgs/doors/14_tuer_standard_rechts_blau.png"/>
+ </tile>
+ <tile id="62">
+  <image width="32" height="32" source="community/cert/imgs/storage/01_palette1.png"/>
+ </tile>
+ <tile id="63">
+  <image width="32" height="32" source="community/cert/imgs/storage/02_palette2.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="96" source="community/cert/imgs/storage/03_shelve.png"/>
+ </tile>
+ <tile id="65">
+  <image width="32" height="96" source="community/cert/imgs/storage/04_shelve2_items.png"/>
+ </tile>
+ <tile id="66">
+  <image width="32" height="96" source="community/cert/imgs/storage/05_shelve2.png"/>
+ </tile>
+ <tile id="67">
+  <image width="512" height="96" source="inventory/dk_transports.png"/>
+ </tile>
+ <tile id="68">
+  <image width="288" height="32" source="structures/mapUtilities.png"/>
+ </tile>
+ <tile id="69">
+  <image width="512" height="256" source="structures/rc3-mullana-colors-128.png"/>
+ </tile>
+ <tile id="70">
+  <image width="512" height="512" source="structures/rc3-mullana-floor-wall.png"/>
+ </tile>
+ <tile id="71">
+  <image width="512" height="512" source="structures/rc3-mullana-functionary.png"/>
+ </tile>
+</tileset>
