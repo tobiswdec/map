13b5e2db72895a72c3b3caa27aaddbc50dba6013
unknown <58555517+tobiswdec@users.noreply.github.com>
2021-03-28 15:07:21 +0200

    AddedTiles

--- tiles_png/SWDEC.tsx
+++ tiles_png/SWDEC.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="swdec" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="swdec/Willkommen2.png" width="32" height="32"/>
+<tileset version="1.5" tiledversion="1.5.0" name="swdec" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="swdec/Willkommen.png" width="96" height="32"/>
 </tileset>
